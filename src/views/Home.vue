c5d3c00315f3ec4068cc1f56b91c6f7afbe53921
FlyingBaum <kristgsus@gmail.com>
2022-01-07 03:54:41 +0100

    Deploying RepoCard component for display for repos

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,51 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1 class="text-6xl m-4"><span class="text-red-500">Krist</span> Baliev</h1>
+    <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+      <div v-for="repo in repos" :key="repo.repoName" class="m-4">
+        <repo-card
+          :repoName="repo.repoName"
+          :repoDescription="repo.repoDesc"
+          :repoStarCount="repo.repoStarCount"
+          :repoUrl="repo.repoUrl"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+import RepoCard from "@/components/RepoCard.vue";
 
-@Component
-export default class Home extends Vue {}
+interface Repo {
+  repoName: string;
+  repoDesc: string;
+  repoStarCount: number;
+  repoUrl: string;
+}
+
+@Component({
+  components: { RepoCard },
+})
+export default class Home extends Vue {
+  repos: Repo[] = [];
+
+  mounted() {
+    axios
+      .get("https://api.github.com/users/FlyingBaum/repos?sort=updated")
+      .then((values) => {
+        values.data.slice(0, 10).forEach((repo: any) => {
+          this.repos.push({
+            repoName: repo.name,
+            repoDesc: repo.desc,
+            repoStarCount: repo.stargazers_count,
+            repoUrl: repo.html_url,
+          });
+        });
+      })
+      .catch(console.error);
+  }
+}
 </script>
