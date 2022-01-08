2df81c9b93ff67d89c2c7b891cbab7995ae18476
FlyingBaum <kristgsus@gmail.com>
2022-01-09 00:15:18 +0100

    Upgraded dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cuteFont: ["Cute Font"],
+      },
+    },
   },
   variants: {
     extend: {},
