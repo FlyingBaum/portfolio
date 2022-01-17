91e4ca3a09be29d5284619b1bf3cbcb6b13e335b
FlyingBaum <kristgsus@gmail.com>
2022-01-17 04:53:49 +0100

    About page update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,24 @@
 <template>
   <div class="about">
-    <h1 class="text-6xl m-4">
-      Hello, I am <span class="text-green-500">Krist</span> Baliev
+    <h1 class="text-5xl mx-5 mb-8 mt-5 text-left">
+      Hey I am <span class="text-green-500">Krist</span> and I like to code!
     </h1>
-    <p class="text-3xl">And I study</p>
+    <div class="text-left m-5 text-2xl">
+      <h2 class="text-3xl font-bold mb-4">Occupation</h2>
+      <p class="mt-2 max-w-6xl">
+        Currently, I study International Media & Computer Science to become a
+        full fledged software developer one day. I still have a long way to go,
+        but I am enjoying the ride so far, especially now because I get to
+        participate in so many projects at university!<br />
+        Check them out
+        <router-link to="/projects" class="text-green-500">here</router-link>.
+      </p>
+    </div>
+    <div class="text-left m-5 text-2xl">
+      <h2 class="text-3xl font-bold mb-4">Interests</h2>
+      <p class="mt-2 max-w-6xl">
+        Besides coding I enjoy playing the piano a lot
+      </p>
+    </div>
   </div>
 </template>
