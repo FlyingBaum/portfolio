b589e3a48940f5e7bb08db50353ea2dd192367b8
FlyingBaum <kristgsus@gmail.com>
2022-01-17 04:54:31 +0100

    Added socials and a bit of styling

--- src/views/Social.vue
+++ src/views/Social.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="social">
-    <h1>This is an about page</h1>
+    <div class="grid grid-cols-1 lg:grid-cols-3 xl:grid-cols-4">
+      <figure class="w-40 m-4">
+        <a href="https://github.com/FlyingBaum" target="_blank">
+          <img
+            :src="require('@/assets/github.svg')"
+            alt="github"
+            class="bg-gray-200 border-gray-400 border-4 rounded-3xl"
+          />
+        </a>
+        <figcaption class="mt-2 text-lg">Github</figcaption>
+      </figure>
+
+      <figure class="w-40 m-4">
+        <a
+          href="https://www.linkedin.com/in/krist-baliev-5a9a54207/"
+          target="_blank"
+        >
+          <img
+            :src="require('@/assets/linkedIn.svg')"
+            alt="linkedIn"
+            class="bg-gray-200 border-gray-400 border-4 rounded-3xl"
+          />
+        </a>
+        <figcaption class="mt-2 text-lg">LinkedIn</figcaption>
+      </figure>
+    </div>
   </div>
 </template>
